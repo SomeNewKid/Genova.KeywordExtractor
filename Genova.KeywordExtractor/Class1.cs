71b38d8e7b4f3304d503378bff445ea9ef14c0be
SomeNewKid <SomeNewKid@hotmail.com>
2025-11-16 06:42:56 +0800

    (1) Remove unused `Genova.KeywordExtractor` project.

--- Genova.KeywordExtractor/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is part of the Genova project licensed under the GNU General Public License v3.0.
-// See the LICENSE file in the project root for more information.
-
-namespace Genova.KeywordExtractor;
-
-public class Class1
-{
-
-}
